369024b5d764f43a501af3506a3b651849448f77
chen <chen@chendeMacBook-Pro.local>
2017-02-06 14:32:32 +0800

    增加一层目录

---
+++ src/module/testm/my/MyApp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="myapp">
+    <img src="../../../assets/logo.png">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from '../../../components/MyHello'
+
+export default {
+  name: 'myapp',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+#myapp {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
